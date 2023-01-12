d68708360c2550e464aa807f675e0ce63da39ddf
mino <hohohosky101@gmail.com>
2023-01-12 16:37:08 +0900

    init spring boot

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,2 +1,14 @@
-package com.example.demo.controller;public class HelloController {
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    public String hello(Model model) {
+        model.addAttribute("age", 30);
+        return "hello";
+    }
 }
